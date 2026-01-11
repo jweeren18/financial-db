5e56daf102b75859f4d8e47d11f2c23d920fa1df
jweeren3 <jweeren3@gatech.edu>
2026-01-11 15:09:36 -0800

    chore: remove root duplicate modules moved into packages

--- init_db.py
+++
@@ -1,7 +0,0 @@
-from db import engine
-from models import Base
-
-if __name__ == '__main__':
-    print('Creating database tables...')
-    Base.metadata.create_all(bind=engine)
-    print('Done. Database created at data.db')
